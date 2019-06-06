230fe03b6ac887e6872be0eff4052dd450a8530a
BanlyTong <banly2602@gmail.com>
2019-06-06 11:59:19 +0700

    alert message and add product images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Product from './pages/Product.js';
